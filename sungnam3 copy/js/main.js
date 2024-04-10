90f4502af82038960a592cef11a5f4241e3ad0b5
bliss93 <157003469+bliss93@users.noreply.github.com>
2024-04-10 12:39:41 +0900

    240410

--- sungnam3 copy/js/main.js
+++ sungnam3 copy/js/main.js
@@ -1,38 +1,48 @@
 /*
     파일명 : main.js
     작성자 : 윤서하
-    작성일 : 24.03.14
+    작성일 : 24.04.10
     설  명 : 메인페이지에서 사용된 jqeury (header 빼고)
 */
-$(document).ready(function(){
+$(document).ready(function(){ 
+    
     $('.show .list > ul > li').on('click', function(){
         $('.show .list > ul > li').removeClass('active')
         $(this).addClass('active')
     });
     
-    const social_swiper = new Swiper('.rental .swiper', { /* 팝업을 감싸는 요소의 class명 */
+    const rental_swiper = new Swiper('.rental .swiper', { /* 팝업을 감싸는 요소의 class명 */
         slidesPerView: 'auto', /* 한번에 보일 팝업의 수 - 모바일 제일 작은 사이즈일때 */
         spaceBetween: 20, /* 팝업과 팝업 사이 여백 */
         centeredSlides:true,
         loop: true,
-        
-
         breakpoints: {
             1640: {
                 slidesPerView: 'auto', /* 사이즈제한 */
                 spaceBetween: 20,
             },
-            
         },
         slideToClickedSlide : true,
-        
-        // autoplay: {  /* 팝업 자동 실행 */
-        // delay: 3000,
-        // disableOnInteraction: true,
-        // },
+        effect: 'coverflow',
+        coverflowEffect: {
+          // rotate:0,
+          slideShadow: false,
+          stretch:70,
+        },
+        speed: 2000,
+        autoplay: {  /* 팝업 자동 실행 */
+        delay: 3000,
+        disableOnInteraction: true,
+        },
+    });
 
-
-
+    $(".rental .swiper").each(function(elem, target){
+      var swp = target.swiper;
+      $(this).hover(function() {
+          swp.autoplay.stop();
+      }, function() {
+          swp.autoplay.start();
+      });
     });
 
     AOS.init({
