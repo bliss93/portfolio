c70e1d9a4bc1fb2a9f31b05c52f4413df6f82579
bliss93 <157003469+bliss93@users.noreply.github.com>
2024-04-21 20:16:23 +0900

    240421

--- portfolio/js/contents.js
+++ portfolio/js/contents.js
@@ -1,6 +1,34 @@
 /*
     파일명 : contents.js
     작성자 : 윤서하
-    작성일 : 2024.04.09
-    설  명 : 각각의 콘텐츠 페이지에서 작동되는 jqeury
+    작성일 : 24.04.21
+    설  명 : 콘텐츠 페이지의 jQuery
 */
+$(document).ready(function(){
+    
+    const content_swiper = new Swiper('.content .swiper', { 
+        autoplay: { 
+            delay: 3000,
+            disableOnInteraction: true,
+        },
+        loop: true, 
+        slidesPerView: 'auto', 
+        spaceBetween: 17, 
+        slideToClickedSlide : true,
+        pagination: { 
+		el: '.content .paging', 
+		clickable: true,  
+		
+	    },
+    });
+    
+    
+
+    AOS.init({
+        offset: 100, 
+        duration: 500, 
+        easing: 'ease', 
+    });
+
+})
+
--- portfolio/js/main.js
+++ portfolio/js/main.js
@@ -1,30 +1,11 @@
 /*
     파일명 : main.js
     작성자 : 윤서하
-    작성일 : 24.03.14
+    작성일 : 24.04.21
     설  명 : 메인페이지에서 사용된 jqeury (header 빼고)
 */
 $(document).ready(function(){
-    const visual_swiper = new Swiper('.visual .swiper', { /* 팝업을 감싼는 요소의 class명 */
-        effect: "fade", /* fade 효과 */
-        autoplay: {  /* 팝업 자동 실행 */
-            delay: 3000,
-            disableOnInteraction: true,
-        },
-        loop: true,  /* 마지막 팝업에서 첫번째 팝업으로 자연스럽게 넘기기 */
-
-        pagination: {  /* 몇개의 팝업이 있는지 보여주는 동그라미 */
-            el: '.visual .paging', /* 해당 요소의 class명 */
-            clickable: true,  /* 클릭하면 해당 팝업으로 이동할 것인지 값 */
-            renderBullet: function (index, className) {   /* paging에 특정 코드 넣기 */
-                return '<span class="page' + (index + 1) + ' '+className+'"><span></span></span>';
-            },
-        },
-    });
-
-
-
-    const product_swiper = new Swiper('.product .swiper', { /* 팝업을 감싼는 요소의 class명 */
+    const content_swiper = new Swiper('.content .swiper', { /* 팝업을 감싼는 요소의 class명 */
         slidesPerView: 'auto', /* 한번에 보일 팝업의 수 - 모바일 제일 작은 사이즈일때 */
         spaceBetween: 20, /* 팝업과 팝업 사이 여백 */
         breakpoints: {
@@ -42,34 +23,11 @@ $(document).ready(function(){
         observeParents: true,
 
         navigation: {  /* 이전, 다음 버튼 */
-        nextEl: '.product .next',  /* 다음 버튼의 클래스명 */
-        prevEl: '.product .prev',  
+        nextEl: '.content .next',  /* 다음 버튼의 클래스명 */
+        prevEl: '.content .prev',  
         },
     
     })
-
-    
-    const social_swiper = new Swiper('.social .swiper', { /* 팝업을 감싼는 요소의 class명 */
-        slidesPerView: 'auto', /* 한번에 보일 팝업의 수 - 모바일 제일 작은 사이즈일때 */
-        spaceBetween: 20, /* 팝업과 팝업 사이 여백 */
-        loop: true,
-
-        breakpoints: {
-            
-            1640: {
-                slidesPerView: 3, /* 사이즈제한 */
-                spaceBetween: 40,
-            },
-            
-        },
-        slideToClickedSlide : true,
-
-        navigation: {  /* 이전, 다음 버튼 */
-        nextEl: '.social .next',  /* 다음 버튼의 클래스명 */
-        prevEl: '.social .prev',  
-        },
-
-    });
     
 
     AOS.init({
